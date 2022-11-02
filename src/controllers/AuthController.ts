42001f4c6a565f0a13ce7651b5189b71040c38f2
Luisszzuniga <l.zuniga@webandcow.com>
2022-11-02 09:15:29 +0100

    update

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,27 @@
 import { Request, Response } from "express";
 import { User } from "../models/User";
+const md5 = require("md5");
 
 class AuthController
 {  
-    login(request: Request, response: Response)
+    async login(request: Request, response: Response)
     {
+        const mail = request.body.mail;
+        const password = request.body.password;
 
+        const user = await User.findOne({ where: { mail: mail }, fields: ['mail', 'password'] });
+
+        if (! user) {
+            response.json({"error": "Adresse mail ou mot de passe incorrect."});
+            return;
+        }
+
+        if (user.password === md5(password)) {
+            response.json({"message": "Connexion réussie."});
+            return;
+        }
+
+        response.json({"error": "Adresse mail ou mot de passe incorrect."});
     }
 
     register(request: Request, response: Response)
